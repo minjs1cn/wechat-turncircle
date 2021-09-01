cf20e335c8ddf98b83f2c3e853d56b26a8f3a780
minjs1cn <qingyang_88@sina.com>
2021-09-01 23:02:31 +0800

    Initial Commit

---
+++ pages/canvas/localStorageWithTime.js
@@ -0,0 +1,35 @@
+var LocalStorage = require('./localStorage')
+
+function getExpireTimestamp(days) {
+  return (
+    new Date(new Date().setHours(23, 59, 59, 1000)).getTime() +
+    days * 24 * 60 * 60 * 1000
+  )
+}
+
+module.exports = {
+  get (key) {
+    const dbData = LocalStorage.get(key)
+
+    if (dbData && dbData._time >= new Date().getTime()) {
+      return dbData.data
+    }
+
+    // 如果过期，即删除
+    LocalStorage.remove(key)
+    return null
+  },
+
+  set (key, value, expires = 0) {
+    const dbData = {
+      data: value,
+      _time: getExpireTimestamp(expires)
+    }
+
+    return LocalStorage.set(key, dbData)
+  },
+
+  remove (key) {
+    LocalStorage.remove(key)
+  }
+}
